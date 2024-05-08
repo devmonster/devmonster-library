8a1182b5dce71c4ca4382af87af5563dfdf95eea
Mark Angelo Miano <mm@dewise.com>
2024-05-08 19:22:24 +0800

    Added new tests for the new Delay and Time To Live, refactor to add the Test Data for Azurite

---
+++ src/AzureStorage/Queue/Queue.Test/TestProject/QueueTests/InsertQueue.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Devmonster.AzureStorage.Test.Queues;
+
+public partial class QueueTests
+{
+    [Fact]
+    public async Task InsertToQueue()
+    {
+        var config = new TestOptions();
+        Startup.GetIConfigurationRoot().Bind(config);
+
+        IQueueRepository queueRepository = new QueueRepository();
+        queueRepository.SetConnectionString(config.ConnectionString);
+
+        CustomerEntity customer = new()
+        {
+            Name = name,
+            Age = age,
+            Email = email
+        };
+
+        try
+        {
+            await queueRepository.AddMessageQueueAsync(queueName, customer);
+            Assert.True(true);
+        }
+        catch
+        {
+            Assert.True(false);
+        }
+    }
+
+    [Fact]
+    public async Task InsertToQueueWithSchedule()
+    {
+        var config = new TestOptions();
+        Startup.GetIConfigurationRoot().Bind(config);
+
+        IQueueRepository queueRepository = new QueueRepository();
+        queueRepository.SetConnectionString(config.ConnectionString);
+
+        CustomerEntity customer = new()
+        {
+            Name = name,
+            Age = age,
+            Email = email
+        };
+
+        try
+        {
+            await queueRepository.AddMessageQueueAsync(queueName, customer, TimeSpan.FromSeconds(30));
+            Assert.True(true);
+        }
+        catch
+        {
+            Assert.True(false);
+        }
+    }
+
+    [Fact]
+    public async Task InsertToQueueImmediatelyWithTimeToLive()
+    {
+        var config = new TestOptions();
+        Startup.GetIConfigurationRoot().Bind(config);
+
+        IQueueRepository queueRepository = new QueueRepository();
+        queueRepository.SetConnectionString(config.ConnectionString);
+
+        CustomerEntity customer = new()
+        {
+            Name = name,
+            Age = age,
+            Email = email
+        };
+
+        try
+        {
+            await queueRepository.AddMessageQueueAsync(queueName, customer, null, TimeSpan.FromSeconds(20));
+
+            Assert.True(true);
+        }
+        catch
+        {
+            Assert.True(false);
+        }
+    }
+
+    [Fact]
+    public async Task InsertToQueueWithDelayAndTimeToLive()
+    {
+        var config = new TestOptions();
+        Startup.GetIConfigurationRoot().Bind(config);
+
+        IQueueRepository queueRepository = new QueueRepository();
+        queueRepository.SetConnectionString(config.ConnectionString);
+
+        CustomerEntity customer = new()
+        {
+            Name = name,
+            Age = age,
+            Email = email
+        };
+
+        try
+        {
+            await queueRepository.AddMessageQueueAsync(queueName, customer, TimeSpan.FromSeconds(20), TimeSpan.FromSeconds(40));
+
+            Assert.True(true);
+        }
+        catch
+        {
+            Assert.True(false);
+        }
+    }
+}
